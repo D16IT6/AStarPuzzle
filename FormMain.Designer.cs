8eb10c1c86ebd9d64baf297ebceaece97f0620d3
TalonEzio <vuthemanh1707@gmail.com>
2023-11-02 02:00:58 +0700

    Add project files.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace AStarPuzzle
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitMain = new System.Windows.Forms.SplitContainer();
+            this.grbLeft = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tblSplitImages = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbSize = new System.Windows.Forms.ComboBox();
+            this.btnChooseImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitMain)).BeginInit();
+            this.splitMain.Panel1.SuspendLayout();
+            this.splitMain.Panel2.SuspendLayout();
+            this.splitMain.SuspendLayout();
+            this.grbLeft.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitMain
+            // 
+            this.splitMain.BackColor = System.Drawing.Color.White;
+            this.splitMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitMain.Location = new System.Drawing.Point(0, 0);
+            this.splitMain.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.splitMain.Name = "splitMain";
+            // 
+            // splitMain.Panel1
+            // 
+            this.splitMain.Panel1.Controls.Add(this.grbLeft);
+            // 
+            // splitMain.Panel2
+            // 
+            this.splitMain.Panel2.Controls.Add(this.groupBox1);
+            this.splitMain.Size = new System.Drawing.Size(932, 540);
+            this.splitMain.SplitterDistance = 614;
+            this.splitMain.SplitterWidth = 6;
+            this.splitMain.TabIndex = 0;
+            // 
+            // grbLeft
+            // 
+            this.grbLeft.Controls.Add(this.panel1);
+            this.grbLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grbLeft.Location = new System.Drawing.Point(0, 0);
+            this.grbLeft.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grbLeft.Name = "grbLeft";
+            this.grbLeft.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grbLeft.Size = new System.Drawing.Size(614, 540);
+            this.grbLeft.TabIndex = 0;
+            this.grbLeft.TabStop = false;
+            this.grbLeft.Text = "Hình ảnh";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tblSplitImages);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(4, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(606, 508);
+            this.panel1.TabIndex = 0;
+            // 
+            // tblSplitImages
+            // 
+            this.tblSplitImages.ColumnCount = 1;
+            this.tblSplitImages.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblSplitImages.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblSplitImages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblSplitImages.Location = new System.Drawing.Point(0, 0);
+            this.tblSplitImages.Margin = new System.Windows.Forms.Padding(0);
+            this.tblSplitImages.Name = "tblSplitImages";
+            this.tblSplitImages.RowCount = 1;
+            this.tblSplitImages.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblSplitImages.Size = new System.Drawing.Size(606, 508);
+            this.tblSplitImages.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.btnChooseImage);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(312, 540);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chức năng";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.cmbSize);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(4, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(304, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Số ô";
+            // 
+            // cmbSize
+            // 
+            this.cmbSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSize.FormattingEnabled = true;
+            this.cmbSize.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.cmbSize.Location = new System.Drawing.Point(72, 15);
+            this.cmbSize.Name = "cmbSize";
+            this.cmbSize.Size = new System.Drawing.Size(213, 29);
+            this.cmbSize.TabIndex = 1;
+            this.cmbSize.SelectedIndexChanged += new System.EventHandler(this.cmbSize_SelectedIndexChanged);
+            // 
+            // btnChooseImage
+            // 
+            this.btnChooseImage.Location = new System.Drawing.Point(190, 494);
+            this.btnChooseImage.Name = "btnChooseImage";
+            this.btnChooseImage.Size = new System.Drawing.Size(99, 41);
+            this.btnChooseImage.TabIndex = 0;
+            this.btnChooseImage.Text = "Chọn ảnh";
+            this.btnChooseImage.UseVisualStyleBackColor = true;
+            this.btnChooseImage.Click += new System.EventHandler(this.btnChooseImage_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 540);
+            this.Controls.Add(this.splitMain);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.splitMain.Panel1.ResumeLayout(false);
+            this.splitMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitMain)).EndInit();
+            this.splitMain.ResumeLayout(false);
+            this.grbLeft.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitMain;
+        private System.Windows.Forms.GroupBox grbLeft;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tblSplitImages;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnChooseImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbSize;
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace AStarPuzzle
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private Panel[,] _panelBoxs;
+        private PictureBox[,] _pictureBoxes;
+        private Image _originalImage;
+        private int _size = 3;
+        private string folderPath = Path.Combine(Application.StartupPath, "Images");
+
+        private void btnChooseImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = @"Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp|All Files|*.*";
+                openFileDialog.Title = @"Chọn ảnh để chạy thuật toán A*";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedFilePath = openFileDialog.FileName;
+                    _originalImage = Image.FromFile(selectedFilePath);
+
+                    _originalImage = LoadImage(_originalImage);
+                }
+            }
+        }
+        public void SaveImage(Image image, string filePath)
+        {
+            Bitmap temp = new Bitmap(image);
+            temp.Save(filePath, ImageFormat.Jpeg);
+        }
+
+        private Image ResizeImage(Image originalImage, int containerWidth, int containerHeight)
+        {
+            if (originalImage.Width < containerWidth || originalImage.Height < containerHeight)
+            {
+                float widthScale = (float)containerWidth / originalImage.Width;
+                float heightScale = (float)containerHeight / originalImage.Height;
+
+                float scale = Math.Max(widthScale, heightScale);
+
+                int newWidth = (int)(originalImage.Width * scale);
+                int newHeight = (int)(originalImage.Height * scale);
+
+                Bitmap resizedImage = new Bitmap(originalImage, newWidth, newHeight);
+                SaveImage(resizedImage,folderPath + "\\CutCenter.jpg");
+
+                //return CutCenterImage(resizedImage, containerWidth, containerHeight);
+                return resizedImage;
+            }
+
+            return originalImage;
+        }
+
+        Image CutCenterImage(Image originalImage, int containerWidth, int containerHeight)
+        {
+            if (containerWidth > containerHeight)
+            {
+                var cutSize = containerHeight;
+                int temp = Math.Abs(containerHeight - containerWidth) / 2;
+                int fromX = temp;
+                var fromY = 0;
+
+                Bitmap croppedImage = new Bitmap(cutSize, cutSize);
+                using (Graphics g = Graphics.FromImage(originalImage))
+                {
+                    // Vẽ phần cắt từ ảnh gốc lên hình ảnh mới
+                    g.DrawImage(
+                        croppedImage,
+                        new Rectangle(0, 0, cutSize, cutSize),
+                        new Rectangle(fromX, fromY, cutSize, cutSize), GraphicsUnit.Pixel
+                        );
+                }
+                SaveImage(croppedImage, folderPath + "\\Crop.jpg");
+
+                return croppedImage;
+            }
+
+            if (containerWidth < containerHeight)
+            {
+                var cutSize = containerWidth;
+                int temp = Math.Abs(containerHeight - containerWidth) / 2;
+
+                int fromX = 0;
+                var fromY = temp;
+
+
+                Bitmap croppedImage = new Bitmap(cutSize, cutSize);
+                using (Graphics g = Graphics.FromImage(croppedImage))
+                {
+                    // Vẽ phần cắt từ ảnh gốc lên hình ảnh mới
+                    g.DrawImage(
+                        originalImage,
+                        new Rectangle(0, 0, cutSize, cutSize),
+                        new Rectangle(fromX, fromY, cutSize, cutSize), GraphicsUnit.Pixel
+                    );
+                }
+                SaveImage(croppedImage, folderPath + "\\Crop.jpg");
+
+                return croppedImage;
+            }
+            return originalImage;
+        }
+
+        private Image LoadImage(Image image)
+        {
+            image = ResizeImage(image, tblSplitImages.Width, tblSplitImages.Height);
+
+            tblSplitImages.Controls.Clear();
+            _panelBoxs = new Panel[_size, _size];
+            _pictureBoxes = new PictureBox[_size, _size];
+
+
+            var cellSize = image.Width / _size;
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    _panelBoxs[i, j] = new Panel()
+                    {
+                        Dock = DockStyle.Fill
+                    };
+                    _pictureBoxes[i, j] = new PictureBox()
+                    {
+                        Dock = DockStyle.Fill,
+                        Width = cellSize,
+                        Height = cellSize,
+                    };
+
+                    Image cellImage = new Bitmap(cellSize, cellSize);
+                    using (Graphics g = Graphics.FromImage(cellImage))
+                    {
+                        g.DrawImage(image,
+                            new Rectangle(0, 0, cellSize, cellSize),
+                            new Rectangle(j * cellSize, i * cellSize, cellSize, cellSize), GraphicsUnit.Pixel);
+                    }
+
+                    _pictureBoxes[i, j].Image = cellImage;
+                    SaveImage(cellImage, folderPath +$"\\cellImages{i}-{j}.jpg");
+
+
+                    _panelBoxs[i, j].Controls.Add(_pictureBoxes[i, j]);
+
+                    tblSplitImages.Controls.Add(_pictureBoxes[i, j], j, i);
+                }
+            }
+
+            return image;
+        }
+
+
+        private void cmbSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbSize.SelectedIndex == -1) return;
+            int size = int.Parse((string)cmbSize.SelectedItem);
+            tblSplitImages.RowCount = tblSplitImages.ColumnCount = size;
+            _size = size;
+
+
+            tblSplitImages.Controls.Clear();
+
+            if (_originalImage != null) LoadImage(_originalImage);
+            ResizeTable(size);
+
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            cmbSize.SelectedItem = "3";
+        }
+
+        void ResizeTable(int size)
+        {
+            var rowHeight = (float)100.0 / size;
+            var rowWidth = (float)100.0 / size;
+            tblSplitImages.RowStyles.Clear();
+            tblSplitImages.ColumnStyles.Clear();
+
+            for (int i = 0; i < size; ++i)
+            {
+                tblSplitImages.RowStyles.Add(new RowStyle(SizeType.Percent, rowHeight));
+                tblSplitImages.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, rowWidth));
+            }
+        }
+    }
+}
